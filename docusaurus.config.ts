a39d50561c86baddc229e787bd8240a086de5ec3
Rémi Ait-Younes <remi@ait-younes.fr>
2026-09-06 15:34:35 +0200

    add favicon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Valrobotik Docs',
   tagline: 'Documentation Valrobotik',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.svg',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
